4961f576d4c3ba659348074e850015fb115857d1
James Balajan <jbalajan@gmail.com>
2016-08-15 17:01:37 +1000

    Delete navbar.js

--- navbar.js
+++
@@ -1,8 +0,0 @@
-var $navLink = $('a.nav');
-
-$(function() {
-    $navLink.click(function(e) {
-        $('li.current').removeClass('current');
-        $(this).parent().addClass('current');
-    });
-});
